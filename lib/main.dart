cd795b5700748784bf6ac83b440a878c1c04aadd
まさたか <shinonome419@icloud.com>
2023-03-10 15:24:41 +0900

    add: MyDatabaseを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_drift_sample/%20src/drift/todos.dart';
 
 void main() {
+  final database = MyDatabase();
   runApp(const MyApp());
 }
 
