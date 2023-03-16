42a8ef337aa4242a486cdb8632750f9a52fa6d1c
まさたか <shinonome419@icloud.com>
2023-03-16 13:58:07 +0900

    update: group関数で他のテストをラップ

--- test/src/drift/todos_test.dart
+++ test/src/drift/todos_test.dart
@@ -22,51 +22,55 @@ void main() {
     await database!.close();
   });
 
-  test('追加した要素の中身がtestになっているかテスト', () async {
-    final id = await database!.addTodo('test');
-    //すべてのList<Todo>を取得する
-    final todo = await database!.allTodoEntries;
+  group("TodoのCRUDテスト", () {
+    test('追加した要素の中身がtestになっているかテスト', () async {
+      //"test"というcontentの要素を追加する
+      await database!.addTodo("test");
 
-    //todoの最初の要素のconntentが"test"であると予想する
-    expect(todo.first.content, 'test');
-    //todoの最初の要素のconntentが"content"であると予想する(失敗する想定)
-    expect(todo.first.content, 'aaaaa');
-  });
+      //すべてのList<Todo>を取得する
+      final todo = await database!.allTodoEntries;
 
-  test("要素の数が増えているかテスト", () async {
-    //追加前のリストを用意
-    final before = await database!.allTodoEntries;
+      //todoの最初の要素のcontentが"test"であると予想する
+      expect(todo.first.content, 'test');
 
-    //要素の数が0個であるかテストする
-    expect(before.length, 0);
+      //todoの最初の要素のcontentが"content"であると予想する(失敗する想定)
+      //expect(todo.first.content, 'aaaaa');
+    });
 
-    //リストに要素を1つ追加する
-    await database!.addTodo("test");
+    test("要素の数が増えているかテスト", () async {
+      //追加前のリストを用意
+      final before = await database!.allTodoEntries;
 
-    //追加後のリストを取得する
-    final after = await database!.allTodoEntries;
+      //要素の数が0個であるかテストする
+      expect(before.length, 0);
 
-    //要素の数が増えているかテストする
-    expect(after.length, 1);
-  });
+      //リストに要素を1つ追加する
+      await database!.addTodo("test");
 
-  test("要素が削除できているかテスト", () async {
-    final todos = await database!.allTodoEntries;
-    await database!.addTodo("test");
+      //追加後のリストを取得する
+      final after = await database!.allTodoEntries;
 
-    final before = await database!.allTodoEntries;
+      //要素の数が増えているかテストする
+      expect(after.length, 1);
+    });
 
-    //要素を追加したため、要素の数が1つかテスト
-    expect(before.length, 1);
+    test("要素が削除できているかテスト", () async {
+      await database!.addTodo("test");
 
-    //要素を1つ削除する
-    await database!.deleteTodo(before[before.length - 1]);
+      final before = await database!.allTodoEntries;
 
-    //要素を削除後のリストを取得
-    final after = await database!.allTodoEntries;
+      //要素を追加したため、要素の数が1つかテスト
+      expect(before.length, 1);
 
-    //要素の数が0個かテスト
-    expect(after.length, 0);
+      //要素を1つ削除する
+      await database!.deleteTodo(before[before.length - 1]);
+
+      //要素を削除後のリストを取得
+      final after = await database!.allTodoEntries;
+
+      //要素の数が0個かテスト
+      expect(after.length, 0);
+    });
   });
 }
 
